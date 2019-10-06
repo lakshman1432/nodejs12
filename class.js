814864125e2de38f8a564cc328c6c67523a00521
lakshman1432 <56225308+lakshman1432@users.noreply.github.com>
2019-10-06 19:28:09 +0530

    Add files via upload
    class declaration

---
+++ class.js
@@ -0,0 +1,24 @@
+class temp{
+
+    constructor(){
+
+        this.name="laksh";
+        this.age=34;
+    }
+     getName(){
+        return this.name;
+
+
+    }
+     getAge()
+{
+    return this.age;
+}
+}
+//var name='laksh'
+//var age=22;
+var d1=new temp();
+console.log(d1.getName());
+console.log(d1.getAge());
+var abc= require(""./demo");
+cal.add();
